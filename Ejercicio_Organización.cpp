22797892e088123a0de48d01ceedbbf8a0aa171e
Cristobal Ocampo Quintero <cocampo5@eafit.edu.co>
2015-09-02 21:44:19 -0500

    Se cambia el ejemplo
    Se ha modificado el ejemplo por una más sencillo

--- Ejercicio_Organización.cpp
+++ Ejercicio_Organización.cpp
@@ -1,12 +1,12 @@
 #include "stdafx.h"
 
-void saludo(){
-	cout << "Hola Mundo" << endl;
+int suma(int i,int j){
+	return i + j;
 }
 
 
 int main(int argc, char* argv[]){
-	saludo();
+	suma(5,4); //Aquí se hace push de los parámetros y se continúa con un CALL
+	system("pause");
 	return 0;
 }
-
